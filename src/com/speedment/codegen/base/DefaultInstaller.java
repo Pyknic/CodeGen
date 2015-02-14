72532fca597bd98ab986fa6e9f9de1456093a916
emilforslund <emil.forslund@pyknic.se>
2015-02-13 19:48:03 -0800

    Changd implementation of DefaultInstaller

--- src/com/speedment/codegen/base/DefaultInstaller.java
+++ src/com/speedment/codegen/base/DefaultInstaller.java
@@ -1,10 +1,9 @@
 package com.speedment.codegen.base;
 
 import java.util.AbstractMap.SimpleImmutableEntry;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Stream;
 
 /**
@@ -12,20 +11,20 @@ import java.util.stream.Stream;
  * @author Emil Forslund
  */
 public class DefaultInstaller implements Installer {
-	private final Set<Map.Entry<Class<?>, Class<? extends CodeView>>> modelToView;
+	private final Map<Class<?>, Class<? extends CodeView>> modelToView;
 	
 	public DefaultInstaller() {
-		modelToView = new HashSet<>();
+		modelToView = new HashMap<>();
 	}
 
 	@Override
 	public <M, V extends CodeView<M>> void install(Class<M> model, Class<V> view) {
-		modelToView.add(new SimpleImmutableEntry<>(model, view));
+		modelToView.put(model, view);
 	}
 
 	@Override
 	public Optional<CodeView> withOne(Class<?> model) {
-		for (final Map.Entry<Class<?>, Class<? extends CodeView>> e : modelToView) {
+		for (final Map.Entry<Class<?>, Class<? extends CodeView>> e : modelToView.entrySet()) {
 			if (e.getKey().isAssignableFrom(model)) {
 				return Optional.of(Installer.create(e.getValue()));
 			}
@@ -36,7 +35,7 @@ public class DefaultInstaller implements Installer {
 	@Override
 	public Stream<CodeView> withAll(Class<?> model) {
 		final Stream.Builder<CodeView> stream = Stream.builder();
-		modelToView.stream()
+		modelToView.entrySet().stream()
 			.filter(e -> e.getKey().isAssignableFrom(model))
 			.forEach((e) -> {
 				stream.add(Installer.create(e.getValue()));
