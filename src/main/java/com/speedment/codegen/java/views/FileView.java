8ad1e12f426eaafc8f61729c1d0381bee4004791
emilforslund <emil.forslund@pyknic.se>
2015-03-13 10:18:06 +0100

    Removed unused import

--- src/main/java/com/speedment/codegen/java/views/FileView.java
+++ src/main/java/com/speedment/codegen/java/views/FileView.java
@@ -17,7 +17,6 @@
 package com.speedment.codegen.java.views;
 
 import static com.speedment.codegen.Formatting.*;
-import static com.speedment.util.CodeCombiner.*;
 import com.speedment.codegen.base.CodeGenerator;
 import com.speedment.codegen.base.CodeView;
 import com.speedment.codegen.base.DependencyManager;
